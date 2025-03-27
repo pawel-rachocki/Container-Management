f4bad29da81dde00d5e25444fd2ba944e6661f98
Pawel <pawel.logo@gmail.com>
2025-03-27 18:54:12 +0100

    Added Container class & subclasses

---
+++ Container.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Container
+{
+
+public double LoadWeight { get; set; } // kg
+public double Height { get; set; }     // cm
+public double Weight { get; set; }     // kg
+public double Depth { get; set; }      // cm
+public double MaxLoad { get; set; }    // kg
+public string SerialNum { get; private set; } // ustawimy później w generowaniu numeru
+
+    public Container(double loadWeight, double height, double weight, double depth, double maxLoad)
+    {
+    LoadWeight = loadWeight;
+    Height = height;
+    Weight = weight;
+    Depth = depth;
+    MaxLoad = maxLoad;
+
+    // TODO: Serial number generation
+    }
+}
